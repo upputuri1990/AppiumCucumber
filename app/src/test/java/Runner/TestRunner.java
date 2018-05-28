f33a2c2d6c7f81dde44b7e23825f7853172ff452
upputuri1990 <upputuri1990@gmail.com>
2018-05-28 13:33:30 -0500

    new andropid studio project

---
+++ app/src/test/java/Runner/TestRunner.java
@@ -0,0 +1,12 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "feature files")
+public class TestRunner {
+
+
+}
